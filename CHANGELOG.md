b85b908bd5fadcd847099f86e041c34ec2bec42b
blasanka <blasanka95@gmail.com>
2020-03-04 16:13:43 +0530

    example added

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
-## [0.1.0] - Stylable Calendar.
+## [0.1.0] - Bare minimum calendar.
 
 - Stylable Calendar with basic functionality.
---
+++ example/stylable_calendar.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:stylable_calendar/stylable_calendar.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Stylable Calendar example",
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => new _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Demo"),
+      ),
+      body: ListView(
+        children: <Widget>[
+          StylableCalendar(
+            specialDays: [20, 13, 6, 10],
+            highlightedDays: [1, 4, 5],
+            isLoading: false,
+            primaryColor: Theme.of(context).primaryColor,
+            primaryColorDark: Theme.of(context).primaryColorDark,
+            secondaryColor: Theme.of(context).primaryColorLight,
+            // in this case to animate highlighted dots
+            selectedDate: (DateTime date) {
+              print(date);
+            },
+            onNext: (DateTime date) {
+              print(date);
+            },
+            onPrevious: (DateTime date) {
+              print(date);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
